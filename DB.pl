c32b097712eaa33fc5296ca065ce5c530e3fd483
Abner2111 <38987646+Abner2111@users.noreply.github.com>
2022-10-21 19:46:02 -0600

    create DB

---
+++ DB.pl
@@ -0,0 +1,20 @@
+
+%50 sustantivos
+%10 verbos
+%
+%gramaticas->BNF.db
+%banco de verbos, frases->DB.db
+%logica(unir base de datos con gramaticas, obtener input de usuario,interfaz)->Logic.db
+
+%saludo(saludoespanol, saludoingles)
+saludo('Pura vida', 'Everything going good').
+saludo('Tuanis','Everything going good').
+%despedida(despedidaespanol, despedidaingles)
+despedida('Chao', 'Bye Bye').
+despedida('Nos vemos', 'See you later').
+%frase(fraseespanol, )
+frase('Cuantos años tienes?','How old are you?').
+
+%verbos
+
+%sustantivos
