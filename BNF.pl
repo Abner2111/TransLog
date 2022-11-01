73d25b7189fae575e3423202a643df83d65cae1d
Apollo13-0 <calderon.ig08@estudiantec.cr>
2022-10-31 22:09:47 -0600

    fix sintagma

--- BNF.pl
+++ BNF.pl
@@ -1,7 +1,7 @@
-?-consult('DB.pl').
+:-include('DB.pl').
 sintagma_verbal(primera,singular,S0e,S0i,S):-verbo(primera, singular,S0e,S0i,S).
 %sintagma_verbal(segunda,singular,S0,S):-
-sintagma_nominal(S0,S,I0,I):-determinante(S0, S1, I0, I1),sustantivo(S1, S, I1, I).
+%sintagma_nominal(S0,S,I0,I):-determinante(S0, S1, I0, I1),sustantivo(S1, S, I1, I).
 
 
 % Sintagma nominal
@@ -9,15 +9,15 @@ sintagma_nominal(S0,S,I0,I):-determinante(S0, S1, I0, I1),sustantivo(S1, S, I1, 
 
 % Formado por un sustantivo o pronombre unico
 
-sintagma_nominal(ES, EN):- pronombre(GENERO, NUMERO, ES, EN).
-sintagma_nominal(ES, EN):- sustantivo(GENERO, NUMERO, ES, EN).
+sintagma_nominal(ES, EN):- pronombre(GENERO, NUMERO, ES, EN, S).
+sintagma_nominal(ES, EN):- sustantivo(GENERO, NUMERO, ES, EN, S).
 
 % Formado por un término que funcione como núcleo acompañado de otras palabras que lo modifican
 
-sintagma_nominal(ES, EN):- determinante(GENERO, NUMERO, ES, EN), sustantivo(GENERO, NUMERO, ES, EN). 
-sintagma_nominal(ES, EN):- determinante(GENERO, NUMERO, ES, EN), sustantivo(GENERO, NUMERO, ES, EN), adjetivo(GENERO, NUMERO, ES, EN).
-sintagma_nominal(ES, EN):- determinante(GENERO, NUMERO, ES, EN), adjetivo(GENERO, NUMERO, ES, EN), sustantivo(GENERO, NUMERO, ES, EN).
-sintagma_nominal(ES, EN):- sustantivo(GENERO, NUMERO, ES, EN), adjetivo(GENERO, NUMERO, ES, EN).
-sintagma_nominal(ES, EN):- adjetivo(GENERO, NUMERO, ES, EN), sustantivo(GENERO, NUMERO, ES, EN).
+sintagma_nominal(ES, EN):- determinante(GENERO, NUMERO, ES, EN, S), sustantivo(GENERO, NUMERO, ES, EN, S). 
+sintagma_nominal(ES, EN):- determinante(GENERO, NUMERO, ES, EN, S), sustantivo(GENERO, NUMERO, ES, EN, S), adjetivo(GENERO, NUMERO, ES, EN, S).
+sintagma_nominal(ES, EN):- determinante(GENERO, NUMERO, ES, EN, S), adjetivo(GENERO, NUMERO, ES, EN, S), sustantivo(GENERO, NUMERO, ES, EN, S).
+sintagma_nominal(ES, EN):- sustantivo(GENERO, NUMERO, ES, EN, S), adjetivo(GENERO, NUMERO, ES, EN, S).
+sintagma_nominal(ES, EN):- adjetivo(GENERO, NUMERO, ES, EN, S), sustantivo(GENERO, NUMERO, ES, EN, S).
 
 % Sintagma verbal
